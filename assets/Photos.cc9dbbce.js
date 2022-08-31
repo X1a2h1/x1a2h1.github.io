133c5f9530b4f77e07ddc8016de8fd75dea65842
Riecko <51504960+X1a2h1@users.noreply.github.com>
2022-09-01 02:26:22 +0800

    Add files via upload

---
+++ assets/Photos.cc9dbbce.js
@@ -0,0 +1,1 @@
+import{$ as o,a1 as t,a2 as e}from"./index.d8811b67.js";const s={name:"Photos"};function a(n,c,r,p,_,f){return t(),e("h1",null,"this me photos")}const l=o(s,[["render",a]]);export{l as default};
